d4b19764c4b57e6e343c61f25f910ab01d1ab7ad
Daniel <your.email@example.com>
2023-09-04 08:11:14 -0400

    problem solved

--- Array + String/0Methed.py
+++ Array + String/0Methed.py
@@ -1,6 +1,12 @@
 sample_list = [1, 2, 3, "apple", "banana", True, False, 4.5, ["nested", "list"]]
 copied_sample_list = sample_list.copy()
 
+#        J
+#        |
+#2t3o11g2s
+#      |
+#      I
+#output: ttooogggggggggggss
 copied_sample_list[4] = "banhana"
 print(sample_list)
 print(copied_sample_list)
