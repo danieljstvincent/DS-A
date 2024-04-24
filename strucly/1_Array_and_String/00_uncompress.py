460209142ed120a4065e61373ca17673a5f4ea80
Daniel <your.email@example.com>
2024-04-23 22:24:29 -0400

    umcompress question created

---
+++ strucly/1_Array_and_String/00_uncompress.py
@@ -0,0 +1,8 @@
+def uncompress(s):
+  pass # todo
+
+print(uncompress("2c3a1t")) # -> 'ccaaat'
+print(uncompress("4s2b")) # -> 'ssssbb'
+print(uncompress("2p1o5p")) # -> 'ppoppppp'
+print(uncompress("3n12e2z")) # -> 'nnneeeeeeeeeeeezz'
+print(uncompress("127y")) # -> 'yyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyy'
