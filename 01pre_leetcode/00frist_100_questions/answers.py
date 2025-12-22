bac0dd76890cd709f2f38823e678a1ca6c725f53
danielstvincent <dannystvincent@gmail.com>
2025-12-22 13:33:26 -0500

    Question 3 completed

--- 01pre_leetcode/00frist_100_questions/answers.py
+++ 01pre_leetcode/00frist_100_questions/answers.py
@@ -10,6 +10,18 @@ def total_amount(a, b):
 
 #question03
 
+def quesiton3(a, b):
+    sum_all = a + b
+    difference_all = a - b
+    product_all = a ** b
+    quotient = a % b
 
+    all_totals = [sum_all, difference_all, product_all, quotient]
+
+  
+
+
+    return all_totals 
 
 print(total_amount(3, 100))
+print(quesiton3(100, 100))
